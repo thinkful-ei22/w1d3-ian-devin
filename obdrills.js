1ff0f1b05279c760976998c9abc64000b88b5beb
dghero <devingreyhero@gmail.com>
2018-06-13 11:07:02 -0500

    Fix minor formatting issues

--- obdrills.js
+++ obdrills.js
@@ -1,18 +1,23 @@
-function createObject(){
-    obj = {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello: function(){
-            return "hello";
-        }
+'use strict';
+
+function createObject(obj){
+  obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello';
     }
+  };
 }
 
 function updateObject(obj) {
-    obj.foo = 'foo';
-    obj.bar = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
-    return obj;
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
 }
+
+
+
