dc775bf80175c81e82ee94706fbffdbb32008b72
Ian Beihl <ianbeihl@gmail.com>
2018-06-13 09:03:10 -0700

    first two

---
+++ obdrills.js
@@ -0,0 +1,18 @@
+function createObject(){
+    obj = {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function(){
+            return "hello";
+        }
+    }
+}
+
+function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+}
