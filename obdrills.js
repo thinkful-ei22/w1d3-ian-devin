bc3b2a0f4666b39ed55baf0611647f95d0bb8ba7
dghero <devingreyhero@gmail.com>
2018-06-13 11:08:47 -0500

    Complete personMaker()

--- obdrills.js
+++ obdrills.js
@@ -19,5 +19,16 @@ function updateObject(obj) {
   return obj;
 }
 
-
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
 
