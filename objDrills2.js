e5ce8d3272a862f6459f25c93f99ec762f577e1e
Ian Beihl <ianbeihl@gmail.com>
2018-06-13 12:24:05 -0700

    completed 5-6

--- objDrills2.js
+++ objDrills2.js
@@ -32,14 +32,18 @@ let hobbit = {
 };
 console.log(hobbit.meals[3]);
 
+//Drill 4/5
+
 let solaire = {
   name: 'Solaire',
-  jobTitle: 'Warrior of Sunlight'
+  jobTitle: 'Warrior of Sunlight',
+  boss: 'god'
 };
 
 let bill = {
   name: 'Bill Nye',
-  jobTitle: 'Science Guy'
+  jobTitle: 'Science Guy',
+  boss: 'mother earth'
 };
 
 let mario  = {
@@ -49,14 +53,55 @@ let mario  = {
 
 let luigi = {
   name: 'Luigi Mario',
-  jobTitle: 'Green Mario'
+  jobTitle: 'Green Mario',
+  boss: 'Mario'
+
 };
 
 let link  = {
   name: 'Link',
-  jobTitle: 'Hero of Time'
+  jobTitle: 'Hero of Time',
+  boss: 'Zelda'
 };
 
 let characterArray = [solaire, bill, mario, luigi, link];
 
-characterArray.forEach(char => console.log(`Name: ${char.name}\nJob:  ${char.jobTitle}`));
+characterArray.forEach(function(char){
+  if(char.boss == undefined){
+    console.log(`${char.jobTitle} ${char.name} doesn't report to anybody.`)
+  }
+  else {
+  console.log(`${char.jobTitle} ${char.name} reports to ${char.boss}`);
+  }
+})
+
+//Drill 6
+
+let cypher = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  default: ' ',
+  decodeWord: function(word){
+    if(this[word[0]] == undefined){
+      return this.default;
+    }
+    else{
+      return word[this[word[0]]];
+    }
+  },
+  decodeWords: function(words){
+    return words.split(' ').map(word => this.decodeWord(word)).join('');
+  }
+};
+
+console.log(cypher.decodeWords('craft block argon meter bells brown croon droop'));
+
+//Drill 7
+
+
+
+
+
+
