ad7b24d188de44fc97c49173cd025f3aa250913c
dghero <devingreyhero@gmail.com>
2018-06-13 11:25:29 -0500

    Complete enrollInSummerSchool()

--- advObjDrills.js
+++ advObjDrills.js
@@ -4,3 +4,9 @@ function makeStudentsReport(data) {
   return data.map(record => `${record.name}: ${record.grade}`);
 }
 
+function enrollInSummerSchool(students) {
+  return students.map(function(record){
+    record.status = 'In Summer school';
+    return record;
+  });
+}
