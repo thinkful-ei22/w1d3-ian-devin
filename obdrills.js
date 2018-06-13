486b94c07430c92d4bf043c6ba45d2d8bcd171b3
dghero <devingreyhero@gmail.com>
2018-06-13 11:11:14 -0500

    Complete keyDeleter()

--- obdrills.js
+++ obdrills.js
@@ -32,3 +32,16 @@ function personMaker() {
   return person;
 }
 
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+  
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
